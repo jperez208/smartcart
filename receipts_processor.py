0aa2a10c2a17e893f7da5555118b5d0ca3e44ea8
i.void_warranties <jperez208@live.com>
2026-07-26 12:44:19 -0600

    Update receipts_processor.py

--- receipts_processor.py
+++ receipts_processor.py
@@ -68,11 +68,11 @@ def process_receipts():
 
         # preprocess
     #Old image processor
-       ''' gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-        gray = cv2.fastNlMeansDenoising(gray)
-        gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]'''
-        # preprocess
+       # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+       # gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+       # gray = cv2.fastNlMeansDenoising(gray)
+       # gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+       # preprocess
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
         gray = cv2.fastNlMeansDenoising(gray)
