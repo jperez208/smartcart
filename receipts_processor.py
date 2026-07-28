f9ebfa51cce689cf3d74ada390e66230b65d7a7b
i.void_warranties <jperez208@live.com>
2026-07-27 21:28:03 -0600

    Update receipts_processor.py

--- receipts_processor.py
+++ receipts_processor.py
@@ -68,17 +68,17 @@ def process_receipts():
 
         # preprocess
     #Old image processor
-       # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-       # gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
-       # gray = cv2.fastNlMeansDenoising(gray)
-       # gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-       # preprocess
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
         gray = cv2.fastNlMeansDenoising(gray)
+        gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+       # preprocess
+       # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+       # gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+       #gray = cv2.fastNlMeansDenoising(gray)
         
         # FIXED: Replaced Otsu with Adaptive Thresholding to prevent blacked-out text
-        gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+       # gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
         
         # ----------------------------
         # OCR selection
