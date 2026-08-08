805f70fe3bd3e53eb75ace8c6b11a5e2af0a82a0
i.void_warranties <jperez208@live.com>
2026-08-08 11:55:34 -0600

    Update save_list.py

--- save_list.py
+++ save_list.py
@@ -1,7 +1,11 @@
 import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ITEM_FILE = os.path.join(BASE_DIR, "items.txt")
+
 def append_list():
-  with open(DEBUG_OCR_FILE, "a", encoding="utf-8") as f:
+  with open(ITEM_FILE, "a", encoding="utf-8") as f:
               f.write(f"\n\n--- {filename} ---\n")
               f.write(f"Method: {best_method}, PSM: {best_psm}\n\n")
               for line in lines:
