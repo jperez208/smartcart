95ce40f62846b1df8fe50bc66a7e5fcbe33590ee
i.void_warranties <jperez208@live.com>
2026-08-09 17:00:19 -0600

    Update save_list.py

--- save_list.py
+++ save_list.py
@@ -3,7 +3,13 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ITEM_FILE = os.path.join(BASE_DIR, "items.txt")
 
-def append_list(store):
+def append_list(store, receipt_date, full_address, clean, price):
     with open(ITEM_FILE, "a", encoding="utf-8") as f:
             f.write("Store:")
             f.write(store + "\n")
+            f.write("Address:")
+            f.write(full_address + "\n")
+            f.write("Date:")
+            f.write(receipt_date + "\n")
+            
+                    
