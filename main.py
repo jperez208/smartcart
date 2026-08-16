1018bb42efbd4a9d34da1e0c3beb008685e0801f
i.void_warranties <jperez208@live.com>
2026-08-16 17:00:45 -0600

    Add product comparison to main execution flow

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from db import init_db
 from receipts_processor import process_receipts
 from import_receipts_to_master import run_import
+from compare_products import run_comparison
 
 def main():
     init_db()
     process_receipts()
     run_import()
+    run_comparison()
 
 if __name__ == "__main__":
     main()
