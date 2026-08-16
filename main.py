d63d8e72b2c812d9f673b26a96c172d81e6f1d99
i.void_warranties <jperez208@live.com>
2026-08-16 16:21:38 -0600

    Add import function call to main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from db import init_db
 from receipts_processor import process_receipts
+from import_receipts_to_master import run_import
 
 def main():
     init_db()
     process_receipts()
+    run_import()
 
 if __name__ == "__main__":
     main()
