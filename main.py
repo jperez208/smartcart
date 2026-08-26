6aa1a0ef9975e169911933c260dc97d4978ff8d0
i.void_warranties <jperez208@live.com>
2026-08-25 21:51:19 -0600

    Add create_master_db function call in main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from db import init_db
 from receipts_processor import process_receipts
 from import_receipts_to_master import run_import
 from compare_products import run_comparison
+from create_master_db import create_master_db
 
 def main():
     init_db()
